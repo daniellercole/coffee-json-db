8aaeb54fbb7b359750460b4303f3cacde96f5a59
Danielle Cole <danielle.rose.cole@gmail.com>
2020-10-23 08:50:01 -0400

    first commit

---
+++ db.js
@@ -0,0 +1,122 @@
+{
+  "bases": [{
+    "id": "b1",
+    "name": "Drip Coffee",
+    "category": "Hot",
+    "base-price": 2.5,
+    "type": [
+      {
+        "name": "Blond roast",
+        "price": 0
+      },
+      {
+        "name": "Dark roast",
+        "price": 0
+      },
+      {
+        "name": "Specialty",
+        "price": 0.5
+      }
+    ],
+    "addons": [
+      {
+        "name": "Milk",
+        "price": 0
+      },
+      {
+        "name": "Cream",
+        "price": 0
+      },
+      {
+        "name": "Sugar",
+        "price": 0
+      },
+      {
+        "name": "Flavour shot",
+        "price": 0.5
+      }
+    ]
+  },
+  {
+    "id": "b2",
+    "name": "Espresso",
+    "category": "Hot",
+    "type": [
+      {
+        "name": "Espresso shot",
+        "price": 2.0
+      },
+      {
+        "name": "Flat white",
+        "price": 3.5
+      },
+      {
+        "name": "Latte",
+        "price": 3.5
+      },
+      {
+        "name": "Cappuccino",
+        "price": 3.5
+      }
+    ],
+    "addons": [
+      {
+        "name": "Espresso shot",
+        "price": 1.0
+      },
+      {
+        "name": "Whipped cream",
+        "price": 1.0
+      },
+      {
+        "name": "Syrup",
+        "price": 1.0
+      },
+      {
+        "name": "Specialty",
+        "price": 1.0
+      }
+    ]
+  },
+  {
+    "id": "b3",
+    "name": "Smoothie",
+    "category": "Cold",
+    "type": [
+      {
+        "name": "Strawberry banana",
+        "price": 4
+      },
+      {
+        "name": "Coconut pineapple",
+        "price": 4.5
+      },
+      {
+        "name": "Greens",
+        "price": 5
+      },
+      {
+        "name": "Orange",
+        "price": 4
+      }
+    ],
+    "addons": [
+      {
+        "name": "Greek yogurt",
+        "price": 1.0
+      },
+      {
+        "name": "Fresh ginger, turmeric and cayenne",
+        "price": 1.5
+      },
+      {
+        "name": "Lemon ginger",
+        "price": 1.5
+      },
+      {
+        "name": "Banana",
+        "price": 1.0
+      }
+    ]
+  }]
+}
